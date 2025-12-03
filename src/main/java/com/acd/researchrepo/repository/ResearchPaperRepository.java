dee8431dc04868b2e9caf76fe372ed78e44a6149
r4ppz <johnreyrabosa.f@gmail.com>
2025-12-03 20:07:13 +0800

    fix(repositories): change entity ID types from Long to Integer

--- src/main/java/com/acd/researchrepo/repository/ResearchPaperRepository.java
+++ src/main/java/com/acd/researchrepo/repository/ResearchPaperRepository.java
@@ -4,6 +4,6 @@ import com.acd.researchrepo.model.ResearchPaper;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ResearchPaperRepository extends JpaRepository<ResearchPaper, Long> {
+public interface ResearchPaperRepository extends JpaRepository<ResearchPaper, Integer> {
 
 }
