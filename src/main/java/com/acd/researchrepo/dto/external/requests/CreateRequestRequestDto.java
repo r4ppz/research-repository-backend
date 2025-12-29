54beef6437bac93b23104886cfe4cde3ada3915b
r4ppz <johnreyrabosa.f@gmail.com>
2025-12-29 14:48:20 +0800

    fix(dto): add @Jacksonized to CreateRequestRequestDto

--- src/main/java/com/acd/researchrepo/dto/external/requests/CreateRequestRequestDto.java
+++ src/main/java/com/acd/researchrepo/dto/external/requests/CreateRequestRequestDto.java
@@ -2,8 +2,10 @@ package com.acd.researchrepo.dto.external.requests;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.extern.jackson.Jacksonized;
 
 @Getter
+@Jacksonized
 @Builder
 public class CreateRequestRequestDto {
     private Integer paperId;
