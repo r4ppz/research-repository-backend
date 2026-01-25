5819fe7700ddab7c3348601460b0ba240e8976d5
r4ppz <johnreyrabosa.f@gmail.com>
2026-01-25 20:31:43 +0800

    refactor(mapper): eliminate code duplication in user dto mapping

--- src/main/java/com/acd/researchrepo/mapper/UserMapper.java
+++ src/main/java/com/acd/researchrepo/mapper/UserMapper.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserMapper {
 
-    DepartmentMapper departmentMapper;
+    private final DepartmentMapper departmentMapper;
 
     public UserMapper(DepartmentMapper departmentMapper) {
         this.departmentMapper = departmentMapper;
@@ -20,27 +20,18 @@ public class UserMapper {
             return null;
         }
 
-        // NOTE: I know the way I wrote this is... ahh
-        boolean haveDepartment = user.getRole().equals(UserRole.DEPARTMENT_ADMIN);
-        if (haveDepartment) {
-            return UserDto.builder()
-                    .userId(user.getUserId())
-                    .email(user.getEmail())
-                    .fullName(user.getFullName())
-                    .role(user.getRole())
-                    .department(departmentMapper.toDto(user.getDepartment()))
-                    .profilePictureUrl(user.getProfilePictureUrl())
-                    .build();
-        } else {
-            return UserDto.builder()
-                    .userId(user.getUserId())
-                    .email(user.getEmail())
-                    .fullName(user.getFullName())
-                    .role(user.getRole())
-                    .department(null)
-                    .profilePictureUrl(user.getProfilePictureUrl())
-                    .build();
+        UserDto.UserDtoBuilder builder = UserDto.builder()
+                .userId(user.getUserId())
+                .email(user.getEmail())
+                .fullName(user.getFullName())
+                .role(user.getRole())
+                .profilePictureUrl(user.getProfilePictureUrl());
 
+        // If its a DEPARTMENT_ADMIN include its department, if its not then dont
+        if (UserRole.DEPARTMENT_ADMIN.equals(user.getRole()) && user.getDepartment() != null) {
+            builder.department(departmentMapper.toDto(user.getDepartment()));
         }
+
+        return builder.build();
     }
 }
